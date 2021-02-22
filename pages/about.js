099ca35284f6ebb6a3bf23c70ee3ce552ead59c4
CasperCarver <pigulevsky.gleb@ya.ru>
2021-02-22 19:37:45 +0300

    Initial setup Nextjs.

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const About = () => {
+    return (
+        <div>
+            <Head>
+                <title>
+                    Webdev News
+                </title>
+                <meta name='keywords' constent='web development, programming' />
+            </Head>
+            <h1>About</h1>
+        </div>
+    );
+};
+
+export default About;
